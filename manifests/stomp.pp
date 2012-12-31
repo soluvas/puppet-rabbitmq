967ca8da46fb11251a6f29f02c170c8832365da9
Hendy Irawan <hendy@soluvas.com>
2012-12-31 11:08:06 +0700

    rabbitmq

--- manifests/stomp.pp
+++ manifests/stomp.pp
@@ -13,45 +13,45 @@ class rabbitmq::stomp (
   $stomp_tcp_listener_port = 6163)
 {
 
-  file { rabbitmq-cowboy:
-  	path    => "/usr/lib/rabbitmq/lib/rabbitmq_server-${rabbitmq::dir_version}/plugins/cowboy-0.5.0-rmq2.8.2-git4b93c2d.ez",
-  	source  => 'puppet:///modules/rabbitmq/cowboy-0.5.0-rmq2.8.2-git4b93c2d.ez',
-    mode    => 0644,
-    group   => 'root',
-    owner   => 'root',
-    require => Package['rabbitmq-server'],
-  }
-  file { rabbitmq-sockjs:
-  	path    => "/usr/lib/rabbitmq/lib/rabbitmq_server-${rabbitmq::dir_version}/plugins/sockjs-0.2.1-rmq2.8.2-gitfa1db96.ez",
-  	source  => 'puppet:///modules/rabbitmq/sockjs-0.2.1-rmq2.8.2-gitfa1db96.ez',
-    mode    => 0644,
-    group   => 'root',
-    owner   => 'root',
-    require => Package['rabbitmq-server'],
-  }
-  file { rabbitmq-web-stomp:
-  	path    => "/usr/lib/rabbitmq/lib/rabbitmq_server-${rabbitmq::dir_version}/plugins/rabbitmq_web_stomp-2.8.2.ez",
-  	source  => 'puppet:///modules/rabbitmq/rabbitmq_web_stomp-2.8.2.ez',
-    mode    => 0644,
-    group   => 'root',
-    owner   => 'root',
-    require => Package['rabbitmq-server'],
-  }
-  file { '/etc/rabbitmq/enabled_plugins':
-    mode    => 0644,
-    group   => 'root',
-    owner   => 'root',
-    source  => 'puppet:///modules/rabbitmq/enabled_plugins',
-    require => File['rabbitmq-cowboy', 'rabbitmq-sockjs', 'rabbitmq-web-stomp'],
-    notify  => Service['rabbitmq-server'],
-  }
-  file { '/etc/rabbitmq/rabbitmq.config':
-    mode    => 0644,
-    group   => 'root',
-    owner   => 'root',
-    content => template('rabbitmq/rabbitmq.config-stomp.erb'),
-    require => File['rabbitmq-cowboy', 'rabbitmq-sockjs', 'rabbitmq-web-stomp'],
-    notify  => Service['rabbitmq-server'],
-  }
+#  file { rabbitmq-cowboy:
+#    path    => "/usr/lib/rabbitmq/lib/rabbitmq_server-${rabbitmq::dir_version}/plugins/cowboy-0.5.0-rmq2.8.2-git4b93c2d.ez",
+#    source  => 'puppet:///modules/rabbitmq/cowboy-0.5.0-rmq2.8.2-git4b93c2d.ez',
+#    mode    => 0644,
+#    group   => 'root',
+#    owner   => 'root',
+#    require => Package['rabbitmq-server'],
+#  }
+#  file { rabbitmq-sockjs:
+#    path    => "/usr/lib/rabbitmq/lib/rabbitmq_server-${rabbitmq::dir_version}/plugins/sockjs-0.2.1-rmq2.8.2-gitfa1db96.ez",
+#    source  => 'puppet:///modules/rabbitmq/sockjs-0.2.1-rmq2.8.2-gitfa1db96.ez',
+#    mode    => 0644,
+#    group   => 'root',
+#    owner   => 'root',
+#    require => Package['rabbitmq-server'],
+#  }
+#  file { rabbitmq-web-stomp:
+#    path    => "/usr/lib/rabbitmq/lib/rabbitmq_server-${rabbitmq::dir_version}/plugins/rabbitmq_web_stomp-2.8.2.ez",
+#    source  => 'puppet:///modules/rabbitmq/rabbitmq_web_stomp-2.8.2.ez',
+#    mode    => 0644,
+#    group   => 'root',
+#    owner   => 'root',
+#    require => Package['rabbitmq-server'],
+#  }
+#  file { '/etc/rabbitmq/enabled_plugins':
+#    mode    => 0644,
+#    group   => 'root',
+#    owner   => 'root',
+#    source  => 'puppet:///modules/rabbitmq/enabled_plugins',
+#    require => File['rabbitmq-cowboy', 'rabbitmq-sockjs', 'rabbitmq-web-stomp'],
+#    notify  => Service['rabbitmq-server'],
+#  }
+#  file { '/etc/rabbitmq/rabbitmq.config':
+#    mode    => 0644,
+#    group   => 'root',
+#    owner   => 'root',
+#    content => template('rabbitmq/rabbitmq.config-stomp.erb'),
+#    require => File['rabbitmq-cowboy', 'rabbitmq-sockjs', 'rabbitmq-web-stomp'],
+#    notify  => Service['rabbitmq-server'],
+#  }
 
 }
